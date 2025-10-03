42530b34eb320c0627b047205816dd491d867ecc
mohammademon393 <mdemon357ep@gmail.com>
2025-10-03 21:27:35 +0600

    creat a jsx component

---
+++ src/component/DaisyPriceingCard/DaisyPriceingCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DaisyPriceingCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DaisyPriceingCard;
--- src/component/PriceingOption/PriceingOptions.jsx
+++ src/component/PriceingOption/PriceingOptions.jsx
@@ -10,9 +10,13 @@ const PriceingOptions = ({ pricePromise }) => {
 
       {/* card container */}
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-10'>
-        {priceData.map((priceing) => (
+        {/* {priceData.map((priceing) => (
           <PriceingCard key={priceing.id} priceing={priceing}></PriceingCard>
-        ))}
+        ))} */}
+
+        {
+            
+        }
       </div>
     </div>
   );
